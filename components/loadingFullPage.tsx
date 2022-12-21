661fffe8f9de259392adcca86c35a24098b36c36
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2022-12-21 14:43:26 -0800

    Loading Page Temporary Component

---
+++ components/loadingFullPage.tsx
@@ -0,0 +1,19 @@
+
+// https://www.npmjs.com/package/react-spinners
+import CircleLoader from "react-spinners/CircleLoader";
+import { useState, CSSProperties } from "react";
+
+
+const override: CSSProperties = {
+	animationFillMode: "none"
+};
+
+const loadingFullPage = () => {
+	return (
+		<div className="h-screen grid place-items-center">
+			<CircleLoader color="black" size={150} cssOverride={override}/>
+		</div>
+	);
+}
+
+export default loadingFullPage;
