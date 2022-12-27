950f2b93a695a58d2d3b619c3a2d67d4e556e033
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2022-12-27 17:32:24 -0500

    Homepage Light

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,20 @@
 import Head from "next/head"
+import { Parallax } from "react-scroll-parallax"
 export default function Home() {
 return (
 	<>
 	<Head>
 		<title>Home | JHY Electrical</title>
 	</Head>
-	<div className="h-80 grid place-items-center bg-blue-500">
-		Main Page
+	<div className="h-[127vh]">
+		<div className="h-[49rem] bg-slate-800 relative grid place-items-center -z-[-2]">
+			<Parallax speed={35} className="absolute top-[-2rem]">
+				<img src="/light.png" className="h-[43rem] pointer-events-none select-none" alt="Hanging Light" />
+			</Parallax>
+			<div className="absolute bottom-[8.5rem] -z-[1] text-[7rem] font-bold text-white">
+				JHY ELECTRICAL
+			</div>
+		</div>
 	</div>
 	</>
 )}
