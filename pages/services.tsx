df36329694fbd7efb62423424b1e954b593e76c3
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2022-12-28 15:33:51 -0500

    Services Page Update

--- pages/services.tsx
+++ pages/services.tsx
@@ -1,8 +1,14 @@
+import Head from "next/head";
 const services = () => {
 	return (
+		<>
+		<Head>
+			<title>Services | JHY Electrical</title>
+		</Head>
 		<div>
 			Services Page
 		</div>
+		</>
 	);
 }
 
