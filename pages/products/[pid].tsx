e47a2c8cbb928220b85a23c192c03c9077cf17a8
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2023-05-24 14:36:36 -0400

    product page quantity selector changes

--- pages/products/[pid].tsx
+++ pages/products/[pid].tsx
@@ -1,6 +1,6 @@
 // next
 import { GetServerSideProps } from 'next'
-import { MouseEventHandler, useState } from 'react';
+import { Dispatch, MouseEventHandler, SetStateAction, useEffect, useState } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 // redux
@@ -23,116 +23,158 @@ import { ResidentialIcon, CommercialIcon, IndustrialIcon } from 'components/cate
 import { PayPalWhiteSVG } from 'components/paypalSVG';
 import { Oval } from 'react-loader-spinner';
 
-const AddCartButton = ({ product, quantity }: {product: ProductInterface, quantity: number})=>{
-	// HANDLE CART
-	const dispatch = useDispatch()
+type AddCartButtonProps = { available: boolean, onClick: () => void }
+const AddCartButton = ({ available, onClick }: AddCartButtonProps) => {
 	// ANIMATIONS
 	const [animating, setAnimating] = useState(false)
 	const animationDuration = 1.8 // time in seconds
 	const fallingEdgeAnimationDuration = 1.2
-
-	// ANIMATE + HANDLE CART
-	const handleCartButtonClick = ()=>{
-		if (product.quantity <= 0) {
-			console.error("No more stock")
-			return
-		}
-
-		dispatch(addToCart({ PID: product.firestoreID, product, quantity }))
-		logEvent(analytics(), "add_to_cart", { PID: product.firestoreID })
+	const animateCart = () => {
 		setAnimating(true)
-		setTimeout(()=>{
+		setTimeout(() => {
 			setAnimating(false)
-		}, animationDuration*1000 + fallingEdgeAnimationDuration * 1000)
+		}, animationDuration * 1000 + fallingEdgeAnimationDuration * 1000)
 	}
 
-	return(
+	// ANIMATE + HANDLE CART
+	const handleCartButtonClick = () => {
+		onClick()
+		animateCart()
+	}
+
+	return (
 		<button
-			onClick={ handleCartButtonClick }
-			className={`w-full border-2 p-2 px-4 relative
+			onClick={handleCartButtonClick}
+			className={`w-full border-2 p-2 px-4 relative overflow-hidden
 				bg-white text-blue-600 border-blue-600
-				overflow-hidden transition-transform will-change-transform
-				${(!animating && product.quantity > 0) && "hover:scale-[102%] active:scale-90"}
-				${product.quantity <= 0 && "text-gray-400 disabled:text-blue-300 disabled:border-blue-300"}`}
-			disabled={animating || product.quantity <= 0}
+				transition-transform hover:scale-[102%] active:scale-90
+				disabled:scale-100 ${animating ? "" : "disabled:text-blue-300 disabled:border-blue-300"}`}
+			disabled={animating || !available}
 		>
 			{
-				product.quantity > 0
-				?
-				<>
-					{/* DEFAULT VIEW */}
-					<div
-						className="relative transition-all duration-200 left-[50%]"
-						style={{
-							transform: `scale(${animating ? 0 : 1}) translateX(-50%)`,
-							opacity: animating ? "0" : "1",
-							transitionDelay: animating ? "" : "0.15s",
-							transformOrigin: "left"
-						}}
-					>
-						<div className="inline-flex flex-row items-center gap-x-1 font-bold">
-							<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-								<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-							</svg>
-							Add to Cart
-						</div>
-					</div>
-					
-					{/* shopping cart animation */}
-					{
-						(animating) &&
-						<motion.svg
-							className="w-6 h-6 absolute top-2 right-full"
-							animate={{left: ["-9%", "50%", "50%", "109%"], rotate: [-16, 0, 0, -16], x:"-50%"}}
-							transition={{
-								duration: animationDuration,
-								times: [0.3, 0.4, 0.45, 1],
-								left:{
-									duration: animationDuration,
-									times: [0, 0.45, 0.55, 1],
-									ease: [0.545, -0.6, 0.235, 0.990]
-								}
+				available || animating
+					?
+					<>
+						{/* DEFAULT VIEW */}
+						<div
+							className="relative transition-all duration-200 left-[50%]"
+							style={{
+								transform: `scale(${animating ? 0 : 1}) translateX(-50%)`,
+								opacity: animating ? "0" : "1",
+								transitionDelay: animating ? "" : "0.15s",
+								transformOrigin: "left"
 							}}
-							initial={false}
-							fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
 						>
-							<path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
-						</motion.svg>					
-					}
+							<div className="inline-flex flex-row items-center gap-x-1 font-bold">
+								<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+								</svg>
+								Add to Cart
+							</div>
+						</div>
 
-					{/* ADDED VIEW */}
-					<div
-						className="absolute transition-all left-[50%] top-2 duration-200"
-						style={{
-							transitionDelay: animating ? `${animationDuration}s` : "",
-							opacity: animating ? "1" : "0",
-							transform: `translate(-50%, ${animating ? 0 : 12}px)`
-						}}
-					>
-						Added
-					</div>
-				</>
-				:
-				<div>Out of Stock</div>
+						{/* shopping cart animation */}
+						{
+							(animating) &&
+							<motion.svg
+								className="w-6 h-6 absolute top-2 right-full"
+								animate={{ left: ["-9%", "50%", "50%", "109%"], rotate: [-16, 0, 0, -16], x: "-50%" }}
+								transition={{
+									duration: animationDuration,
+									times: [0.3, 0.4, 0.45, 1],
+									left: {
+										duration: animationDuration,
+										times: [0, 0.45, 0.55, 1],
+										ease: [0.545, -0.6, 0.235, 0.990]
+									}
+								}}
+								initial={false}
+								fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
+							>
+								<path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
+							</motion.svg>
+						}
+
+						{/* ADDED VIEW */}
+						<div
+							className="absolute transition-all left-[50%] top-2 duration-200"
+							style={{
+								transitionDelay: animating ? `${animationDuration}s` : "",
+								opacity: animating ? "1" : "0",
+								transform: `translate(-50%, ${animating ? 0 : 12}px)`
+							}}
+						>
+							Added
+						</div>
+					</>
+					:
+					<div>No more stock</div>
 			}
 		</button>
 	)
 }
 
-const ProductID = ({product}: {product: ProductInterface}) => {
-	const [quantity, setQuantity] = useState(1)
+type QuantityAdjusterProps = { selectedQuantity: number, setSelectedQuantity: Dispatch<SetStateAction<number>>, quantityAvailable: number }
+const QuantityAdjuster = ({ selectedQuantity, setSelectedQuantity, quantityAvailable }: QuantityAdjusterProps) => {
+	const canAddCart = selectedQuantity + 1 <= quantityAvailable
+	const canSubtractCart = selectedQuantity - 1 > 0
+	const subHandler = () => setSelectedQuantity(q => Math.max(q - 1, 1))
+	const addHandler = () => setSelectedQuantity(q => Math.min(q + 1, quantityAvailable))
+	return (
+		<div className="flex flex-row items-center gap-x-4 my-4">
+			Qty
+			<div className="flex flex-row border-2">
+				{/* - icon */}
+				<button className="w-10 h-10 grid place-items-center disabled:text-gray-300"
+					disabled={!canSubtractCart} onClick={subHandler}>
+					<svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+						<path fillRule="evenodd" d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
+					</svg>
+				</button>
+				{/* Quantity */}
+				<span className="w-10 h-10 grid place-items-center">{selectedQuantity}</span>
+				{/* + icon */}
+				<button className="w-10 h-10 grid place-items-center disabled:text-gray-300"
+					disabled={!canAddCart} onClick={addHandler}>
+					<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+					</svg>
+				</button>
+			</div>
+		</div>
+	)
+}
+
+const ProductID = ({ product }: { product: ProductInterface }) => {
 	const router = useRouter()
+	const dispatch = useDispatch()
 	const cart = useSelector((state: { cart: OrderProduct[] }) => state.cart) as OrderProduct[]
 
+	// Quantity Adjuster
+	const quantityInCart = cart.find(p => p.PID == product.firestoreID)?.quantity || 0
+	const availableToAdd = product.quantity - quantityInCart
+	const [selectedQuantity, setSelectedQuantity] = useState(Math.min(availableToAdd, 1)) //either 0 or 1
+	useEffect(() => {
+		setSelectedQuantity(Math.min(selectedQuantity, availableToAdd)) //clamp to below availableToAdd
+	}, [cart])
+
+	// Add to Cart Button
+	const addCartHandler = () => {
+		if (availableToAdd <= 0) return toast.error("No more stock") // should be unreachable code, if button is acting right.
+		dispatch(addToCart({ PID: product.firestoreID, product, quantity: selectedQuantity }))
+		logEvent(analytics(), "add_to_cart", { PID: product.firestoreID })
+	}
+
+	// Buy Now Button
 	const [buyNowButtonLoading, setbuyNowButtonLoading] = useState(false)
 	const buyNow: MouseEventHandler<HTMLButtonElement> = async () => {
 		setbuyNowButtonLoading(true)
-		try{
-			const {redirect_link} = await createPayPalOrderLink([{ PID: product.firestoreID, quantity, product }], true)
+		try {
+			const { redirect_link } = await createPayPalOrderLink([{ PID: product.firestoreID, quantity: selectedQuantity, product }], true)
 			router.push(redirect_link)
 		}
-		catch(e){
-			toast.error((e as Error).message, {theme: "colored"})
+		catch (e) {
+			toast.error((e as Error).message, { theme: "colored" })
 			setbuyNowButtonLoading(false)
 		}
 	}
@@ -152,48 +194,28 @@ const ProductID = ({product}: {product: ProductInterface}) => {
 						<p className="mb-4 text-lg">{product.description}</p>
 						<p className="mb-1 text-lg">{product.quantity} in stock</p>
 						<div className="mb-3">
-							<Price price={product.price} large/>
+							<Price price={product.price} large />
 						</div>
 
 						{/* Categories */}
 						<div className="flex flex-row gap-x-4 mb-3 flex-wrap">
-							{product.residential && <div className="flex flex-row items-center gap-x-2">{<ResidentialIcon className="w-10 h-10"/>} Residential</div>}
-							{product.commercial && <div className="flex flex-row items-center gap-x-2">{<CommercialIcon className="w-10 h-10"/>} Commercial</div>}
-							{product.industrial && <div className="flex flex-row items-center gap-x-2">{<IndustrialIcon className="w-10 h-10"/>} Industrial</div>}
+							{product.residential && <div className="flex flex-row items-center gap-x-2">{<ResidentialIcon className="w-10 h-10" />} Residential</div>}
+							{product.commercial && <div className="flex flex-row items-center gap-x-2">{<CommercialIcon className="w-10 h-10" />} Commercial</div>}
+							{product.industrial && <div className="flex flex-row items-center gap-x-2">{<IndustrialIcon className="w-10 h-10" />} Industrial</div>}
 						</div>
 
 						{/* Quantity Adjuster */}
-						<div className="flex flex-row items-center gap-x-4 my-4">
-							Qty
-							<div className="flex flex-row border-2">
-								{/* - icon */}
-								<button className="w-10 h-10 grid place-items-center disabled:text-gray-300"
-									disabled={quantity - 1 < 1} onClick={() => setQuantity(q => Math.max(q - 1, 1))}>
-									<svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-										<path fillRule="evenodd" d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
-									</svg>
-								</button>
-								{/* Quantity */}
-								<span className="w-10 h-10 grid place-items-center">{Math.min(quantity, product.quantity)}</span>
-								{/* + icon */}
-								<button className="w-10 h-10 grid place-items-center disabled:text-gray-300"
-									disabled={quantity + 1 > product.quantity - (cart.find(p => p.PID == product.firestoreID)?.quantity || 0)} onClick={() => setQuantity(q => Math.min(q + 1, product.quantity - (cart.find(p=>p.PID == product.firestoreID)?.quantity || 0)))}>
-									<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-									</svg>
-								</button>
-							</div>
-						</div>
+						<QuantityAdjuster selectedQuantity={selectedQuantity} setSelectedQuantity={setSelectedQuantity} quantityAvailable={availableToAdd} />
 
 						{/* Add Cart */}
-						<AddCartButton product={product} quantity={quantity}/>
+						<AddCartButton available={availableToAdd > 0} onClick={addCartHandler} />
 						<button
-							onClick={buyNow} disabled={product.quantity <= 0}
+							onClick={buyNow} disabled={(selectedQuantity <= 0) || buyNowButtonLoading}
 							className={
 								`mt-2 p-3 w-full relative grid place-items-center
 								font-bold bg-blue-700 text-white 
 								hover:scale-[102%] transition-transform active:scale-[97%]
-								${product.quantity <= 0 ? "disabled:bg-blue-300 disabled:scale-100" : ""}`
+								${selectedQuantity <= 0 ? "disabled:bg-blue-300 disabled:scale-100" : ""}`
 							}
 						>
 							<Oval height={18} width={18} strokeWidth={10} strokeWidthSecondary={10} color="white" secondaryColor="white"
@@ -211,11 +233,11 @@ const ProductID = ({product}: {product: ProductInterface}) => {
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	try{
+	try {
 		const product = await getProductByID(ctx.params!.pid as string) //eslint-disable-line @typescript-eslint/no-non-null-assertion
 		return { props: { product } }
 	}
-	catch(e){
+	catch (e) {
 		return {
 			redirect: {
 				destination: "/products",
