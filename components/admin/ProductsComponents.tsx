d38a8ec43f3427673675c384d46da1fd27a9ea7a
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2023-06-27 13:59:34 -0400

    delete rounder

--- components/admin/ProductsComponents.tsx
+++ components/admin/ProductsComponents.tsx
@@ -2,6 +2,7 @@
 import { CommercialIcon, IndustrialIcon, ResidentialIcon } from "components/categoryIcons"
 import { AnimatePresence, Variants, motion } from "framer-motion"
 import { Oval } from "react-loader-spinner"
+import ProductComponentsCSS from "./ProductsComponents.module.css"
 // firebase
 import { ProductInterface } from "types/product"
 import { Transition } from "@headlessui/react"
@@ -64,7 +65,11 @@ const ProductElement = ({ product, deleteProduct, openEditModal, itemVariants }:
 					deleteActive
 						? <Oval height={24} width={24} strokeWidth={10} color="#28a9fa" secondaryColor="#28a9fa" />
 						: confirmDelete
-							? <svg onClick={deleteThisProduct} className="h-6 w-6 hover:cursor-pointer fill-black stroke-transparent" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"> <path clipRule="evenodd" fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" /> </svg>
+							?
+							<div className="relative">
+								<svg onClick={deleteThisProduct} className="h-6 w-6 hover:cursor-pointer fill-black stroke-transparent" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" > <path clipRule="evenodd" fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" /> </svg>
+								<div className={`absolute inset-0 rounded-full pointer-events-none ${ProductComponentsCSS.confirmDeleteRound}`}></div>
+							</div>
 							: <svg onClick={setConfirmOn} className="w-6 h-6 hover:cursor-pointer fill-transparent stroke-black" aria-hidden="true" strokeWidth={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"> <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /> </svg>
 				}
 			</div>
